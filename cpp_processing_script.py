0942ee30a00742f97c3920fdf40b33d353d1e704
shihche0716 <alex414448@gmail.comgit>
2023-12-17 14:07:28 +0800

    bug fixed

--- cpp_processing_script.py
+++ cpp_processing_script.py
@@ -6,6 +6,7 @@ import os
 import csv
 from matplotlib import gridspec
 from scipy import fft as sp_fft
+from scipy import signal
 import itertools
 import pickle
 
@@ -79,7 +80,7 @@ def resample(x, num, t=None, axis=0, window=None, domain='time'):
                 raise ValueError('window must have the same length as data')
             W = window
         else:
-            W = sp_fft.ifftshift(get_window(window, Nx))
+            W = sp_fft.ifftshift(signal.get_window(window, Nx))
 
         newshape_W = [1] * x.ndim
         newshape_W[axis] = X.shape[axis]
